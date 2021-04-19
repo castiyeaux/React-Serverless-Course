eefc84a1258a5acb4d2eead6d72b05dfbd600827
Andrea Castillo <castiyeauxdev@gmail.com>
2021-04-18 23:06:06 -0500

    playing around with the score via useState and useEffect Hooks

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,12 +1,30 @@
+import { useState, useEffect } from 'react';
 import { StyledGame, StyledScore, StyledTimer, StyledCharacter } from "../styled/Game";
 import { Strong } from "../styled/Random";
 
 export default function Game() {
+    const [score, setScore] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setScore((prevScore) => prevScore + 1);
+        }, 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, [score]);
+
+    
+
     return (
         <StyledGame>
-            <StyledScore>Score: <Strong>0</Strong></StyledScore>
+            <StyledScore>
+                Score: <Strong>{ score }</Strong>
+            </StyledScore>
             <StyledCharacter>A</StyledCharacter>
-            <StyledTimer>Time: <Strong>00:000</Strong></StyledTimer>
+            <StyledTimer>
+                Time: <Strong>00:000</Strong>
+            </StyledTimer>
         </StyledGame>
     );
 }
